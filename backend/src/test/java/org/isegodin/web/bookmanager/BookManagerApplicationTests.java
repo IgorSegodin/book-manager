c0074016a8cee845f17afb6a61dc2afa566d096a
i.segodin <igor.segodin@gmail.com>
2021-06-20 18:36:50 +0300

    Embedded database

--- backend/src/test/java/org/isegodin/web/bookmanager/BookManagerApplicationTests.java
+++ backend/src/test/java/org/isegodin/web/bookmanager/BookManagerApplicationTests.java
@@ -1,32 +1,19 @@
 package org.isegodin.web.bookmanager;
 
-//import com.opentable.db.postgres.embedded.EmbeddedPostgres;
-//import com.opentable.db.postgres.embedded.LiquibasePreparer;
-//import com.opentable.db.postgres.junit.EmbeddedPostgresRules;
-//import com.opentable.db.postgres.junit.PreparedDbRule;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 
-@SpringBootTest(/*classes = TestContext.class*/)
+@SpringBootTest
+@AutoConfigureEmbeddedDatabase(
+        provider = AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY
+)
 class BookManagerApplicationTests {
-
-//    public void DataSource
-
-//    @RegisterExtension
-//    public PreparedDbRule db = EmbeddedPostgresRules.preparedDatabase(
-//            LiquibasePreparer.forClasspathLocation("liqui/master.xml")
-//    );
-
 
     @Test
     void contextLoads() throws IOException {
-
-//        try(EmbeddedPostgres pg = EmbeddedPostgres.start()) {
-//            System.out.println();
-//        }
     }
 
 }
